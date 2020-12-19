fa8291e3b142e750413be509c286f01da358d594
akshayyadav76 <akshayyadav7613@gmail.com>
2020-12-19 22:23:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/ak/flutter_clean_architech/flutter_clean_architech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ak.flutter_clean_architech.flutter_clean_architech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/entities/movie_entity.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+
+
+class MovieEntity extends Equatable{
+
+final int id;
+final String posterPath;
+final String backdropPath;
+final String title;
+final String realaseDate;
+final String overview;
+final num voteAverage;
+
+ const MovieEntity({
+     @required this.id,
+     @required this.posterPath,
+     @required this.backdropPath, 
+     @required this.title, 
+     @required this.realaseDate, 
+     @required this.voteAverage,
+     this.overview, 
+     }) : assert(id !=null,"movie id must not null");
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [id,title];
+
+@override
+ bool get stringify => true;
+   
+}
