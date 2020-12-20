0c21a49216d75645c42e0113dda06d787a7b7808
akshayyadav76 <akshayyadav7613@gmail.com>
2020-12-20 16:52:10 +0530

    added dependency injection getIt

---
+++ lib/di/git_it.dart
@@ -0,0 +1,28 @@
+
+
+
+
+import 'package:flutter_clean_architech/data/core/api_client.dart';
+import 'package:flutter_clean_architech/data/data_source/movie_data_remote_source.dart';
+import 'package:flutter_clean_architech/domain/usecases/get_comming_soon.dart';
+import 'package:flutter_clean_architech/domain/usecases/get_playing_now.dart';
+import 'package:flutter_clean_architech/domain/usecases/get_popular.dart';
+import 'package:flutter_clean_architech/domain/usecases/get_trending.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+
+final getItInstance = GetIt.I;
+
+Future init()async{
+// they used in all our app so singleton 
+  getItInstance.registerLazySingleton<Client>(()=>Client());
+  getItInstance.registerLazySingleton<ApiClient>(()=>ApiClient(getItInstance()));
+  getItInstance.registerLazySingleton<MovieDataRemoteSource>(()=>MovieDataRemoteSourceImpl(getItInstance()));
+
+
+  getItInstance.registerLazySingleton<GetTrending>(()=>GetTrending(getItInstance()));
+  getItInstance.registerLazySingleton<GetPopular>(()=>GetPopular(getItInstance()));
+  getItInstance.registerLazySingleton<GetPlayingNow>(()=>GetPlayingNow(getItInstance()));
+  getItInstance.registerLazySingleton<GetCommingSoon>(()=>GetCommingSoon(getItInstance()));
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_clean_architech/data/core/api_client.dart';
 import 'package:flutter_clean_architech/data/data_source/movie_data_remote_source.dart';
 import 'package:flutter_clean_architech/data/repositories/movie_repository_impl.dart';
+import 'package:flutter_clean_architech/di/git_it.dart' as getIt;
 import 'package:flutter_clean_architech/domain/entities/no_params.dart';
 import 'package:flutter_clean_architech/domain/usecases/get_trending.dart';
 import 'package:http/http.dart';
+import 'package:pedantic/pedantic.dart';
 
 import 'domain/entities/app_error.dart';
 import 'domain/entities/movie_entity.dart';
 
+
 void main()async{
-  ApiClient apiClient =ApiClient(Client());
-  var movieDataRemoteImpl= MovieDataRemoteSourceImpl(apiClient);
-  var obj =MovieRepositoryImpl(movieDataRemoteImpl);
-  //obj.getTranding();
-  GetTrending getTrending =GetTrending(obj);
+  // app won't start untill all the getit instance instialize
+  unawaited(getIt.init());
+
+  // ApiClient apiClient =ApiClient(Client());
+  // var movieDataRemoteImpl= MovieDataRemoteSourceImpl(apiClient);
+  // var obj =MovieRepositoryImpl(movieDataRemoteImpl);
+  // //obj.getTranding();
+  // GetTrending getTrending =GetTrending(obj);
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
 Either<AppError,List<MovieEntity>>response = await getTrending(NoParams());
 response.fold(
   (l){
